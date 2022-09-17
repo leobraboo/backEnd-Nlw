3639ecb4a6945e23d5413b1ce719d83433d9104b
leobraboo <leonardo.l.souza@ufv.br>
2022-09-17 17:04:45 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/games', (request, response) =>{
+   return response.json([]);
+});
+
+app.post('/ads',(request, response) =>{
+   return response.status(201).json([]);
+});
+
+app.get('/games/:id/ads', (request, response) => {
+   return response.json([
+
+
+   ]);
+});
+
+
+app.get('/ads/:id/discord', (request, response) => {
+   return response.json([
+
+
+   ]);
+});
+
+app.listen(3333)
