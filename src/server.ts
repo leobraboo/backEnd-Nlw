13ae762ced5b08eef82ddfb72a9a6c9289a6cdd1
leobraboo <leonardo.l.souza@ufv.br>
2022-09-17 18:07:22 -0300

    routes

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,53 @@
 import express from 'express'
+import { PrismaClient }  from '@prisma/client'
+
 
 const app = express()
 
-app.get('/games', (request, response) =>{
-   return response.json([]);
+const prisma = new PrismaClient({
+   log: ['query']
+});
+
+
+app.get('/games', async (request, response) =>{
+   const games = await prisma.game.findMany({
+      include: {
+         _count: {
+            select: {
+               ads: true,
+            }
+         }
+      }
+   });
+
+   return response.json(games);
 });
 
 app.post('/ads',(request, response) =>{
    return response.status(201).json([]);
 });
 
-app.get('/games/:id/ads', (request, response) => {
-   return response.json([
+app.get('/games/:id/ads', async (request, response) => {
+   const gameId = request.params.id;
+
+   const ads = await  prisma.ad.findMany({
+      select:{
+         id: true,
+         name: true,
+         weekDays: true,
+         useVoiceChannel: true,
+         yearsPlaying: true,
+         hourStart: true,
+         hourEnd: true,
+         createdAt: true,
+      },
+      where: {
+         gameId,
+      }
+   })
 
 
-   ]);
+   return response.json(ads);
 });
 
 
